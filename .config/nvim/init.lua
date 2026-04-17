3a283d4eba03d809da88052126bbc14abac80d34
ianc <iankencruz@gmail.com>
2026-04-17 14:59:40 +1000

    change telescope from [S] to [F]

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -348,16 +348,16 @@ require("telescope").setup({})
 
 local pickers = require("telescope.builtin")
 
-vim.keymap.set("n", "<leader>sp", pickers.builtin, { desc = "[S]earch Builtin [P]ickers" })
-vim.keymap.set("n", "<leader>sb", pickers.buffers, { desc = "[S]earch [B]uffers" })
-vim.keymap.set("n", "<leader>sf", pickers.find_files, { desc = "[S]earch [F]iles" })
-vim.keymap.set("n", "<leader>sw", pickers.grep_string, { desc = "[S]earch Current [W]ord" })
-vim.keymap.set("n", "<leader>sg", pickers.live_grep, { desc = "[S]earch by [G]rep" })
-vim.keymap.set("n", "<leader>sr", pickers.oldfiles, { desc = "[S]earch [R]ecent" })
-vim.keymap.set("n", "<leader>sk", require("telescope.builtin").pickers, { desc = "[S]earch [K]ached Pickers" })
+vim.keymap.set("n", "<leader>fp", pickers.builtin, { desc = "[F]ind Builtin [P]ickers" })
+vim.keymap.set("n", "<leader>fb", pickers.buffers, { desc = "[F]ind [B]uffers" })
+vim.keymap.set("n", "<leader>ff", pickers.find_files, { desc = "[F]ind [F]iles" })
+vim.keymap.set("n", "<leader>fw", pickers.grep_string, { desc = "[F]ind Current [W]ord" })
+vim.keymap.set("n", "<leader>fg", pickers.live_grep, { desc = "[F]ind by [G]rep" })
+vim.keymap.set("n", "<leader>fr", pickers.oldfiles, { desc = "[F]ind [R]ecent" })
+vim.keymap.set("n", "<leader>fk", require("telescope.builtin").pickers, { desc = "[F]earch [K]ached Pickers" })
 
-vim.keymap.set("n", "<leader>sh", pickers.help_tags, { desc = "[S]earch [H]elp" })
-vim.keymap.set("n", "<leader>sm", pickers.man_pages, { desc = "[S]earch [M]anuals" })
+vim.keymap.set("n", "<leader>fh", pickers.help_tags, { desc = "[F]ind [H]elp" })
+vim.keymap.set("n", "<leader>fm", pickers.man_pages, { desc = "[F]ind [M]anuals" })
 
 -- INFO: better statusline
 vim.pack.add({ "https://github.com/nvim-lualine/lualine.nvim" }, { confirm = false })
