3dc60b00ec5232e3ea69cfa75aefef02f26c6cf2
ianc <iankencruz@gmail.com>
2026-08-20 00:48:49 +1000

    update sleek line between new queries

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -9,7 +9,7 @@ return {
       formatters = {
         sleek = {
           command = "sleek",
-          args = { "-U", "true", "-l", "1" },
+          args = { "-U", "true", "-l", "2" },
           stdin = true,
         },
       },
